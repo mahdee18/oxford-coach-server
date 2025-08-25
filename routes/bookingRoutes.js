9aebc7ba0385a6d0bebd1ba601b4ec7c63af8279
mutanim24 <mutanim24@gmail.com>
2025-08-25 05:57:09 -0600

    complete booking summery

--- routes/bookingRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { protect } = require('../middleware/authMiddleware');
-const { createBooking } = require('../controllers/bookingController');
-
-// Create a new booking
-router.post('/', protect, createBooking);
-
-module.exports = router;
